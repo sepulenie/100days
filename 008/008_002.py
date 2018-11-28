2aabe84aef3fd8305da8e224044acbab61981b10
sepulenie <maximreutov@gmail.com>
2018-11-28 16:28:04 +0300

    added input class

--- 008/008_002.py
+++ 008/008_002.py
@@ -5,16 +5,32 @@ db = shelve.open('train_data')
 train_set_len = len(db['train_set'][0])
 examples = [example for example in db['train_set']]
 
+#Входящая информация
+class Input_neuron:
+    def __init__(self, value, inputs_count):
+        self.weights = []
+        i_c = 0
+        while   i_c != inputs_count:
+            self.weights.append(random.random())
+            i_c += 1
+    def changeWeights(self):
+        self.changedWeights = []
+        for w in self.weights:
+            while True:
+                c = random.uniform(0.0000000000000010,-0.0000000000000010)
+                if w + c < 0 or w + c > 1:
+                    pass
+                else:
+                    self.changedWeights.append(w+c)
+                    break
+        self.weights=self.changedWeights
+        print('__________________________\n')
+        print(self.weights)
 
+#Нейрон
 class Neuron:
     def __init__(self, input_e, input_weights):
-        self.value = sum([input_e * input_weight for input_weight in input_weights])
-
-
-
-
-
-
+        self.value = [input_e * input_weight for input_weight in input_weights]
 
 
 '''
@@ -29,4 +45,4 @@ def start_training(inputs_count, neuron_count = 0, layer_count = 0):
             print(round(random.random(), 4))
             n +=1
 '''
-start_training(train_set_len, 3, 1)
+#start_training(train_set_len, 3, 1)
